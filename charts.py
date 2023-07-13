94e95a1ac3be55a6e3754389b9f3916c313826cc
angus-lin <anguslin199@gmail.com>
2023-07-13 19:56:05 -0400

    added garage car chart

---
+++ charts.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def garage_chart(df, columns, coefficients):
+    coefficients[columns.get_loc('GarageCars')]
+    garage_coeff = coefficients[columns.get_loc('GarageCars')]
+
+    values = [garage_coeff * x for x in range(int(df['GarageCars']['min']), int(df['GarageCars']['max']))]
+
+    print(values)
+
+
+    chart_data = pd.DataFrame(
+    {"Number of Garage Spaces": 
+        [x for x in range(
+        int(df['GarageCars']['min']),
+        int(df['GarageCars']['max']))],
+    "Change in Price (USD)": values
+    },
+    )
+
+    st.bar_chart(chart_data, 
+    x = "Number of Garage Spaces")
