49a4cd0a542d66d108795dedb895ee191e1e4e64
rashad arbab <rashad.arbab@gmail.com>
2023-07-13 22:37:02 -0400

    added subheaders

--- house_prices.py
+++ house_prices.py
@@ -27,14 +27,17 @@ if __name__ == "__main__":
 
 
     get_all_input(df_summary_numerical,columns,coefficients,intercept)
-    display_charts(df_summary_numerical, columns, coefficients)
-    
+    st.header("How do different features affect the price of housing?")
+
+
+    st.subheader("Value Added by Exterior Quality")
     hist_data_ext_qual = [46765.50376, -11790.75027, -19896.26241, -15078.49108]
     hist_label_ext_qual = ["Excellent", "Good", "Average", "Fair"]
     ext_qual = pd.DataFrame({"Exterior Quality": hist_label_ext_qual, "$ (USD)": hist_data_ext_qual})
     ax = ext_qual.plot.bar(x="Exterior Quality", y="$ (USD)")
     st.bar_chart(ext_qual, x="Exterior Quality", y="$ (USD)")
 
+    st.subheader("Value Added by Central Air")
     hist_data_central_air = [-2388.683262, 2388.683262]
     hist_label_central_air = ["Available", "Not Available"]
     central_air = pd.DataFrame({"Central Air": hist_label_central_air, "$ (USD)": hist_data_central_air})
@@ -42,19 +45,21 @@ if __name__ == "__main__":
     st.bar_chart(central_air, x="Central Air", y="$ (USD)")
     
     
-
+    st.subheader("Value Added by Heating Quality")
     hist_data_heat = [5181.41053, 514.522098, -756.287888, 3223.430193, -8163.074934]
     hist_label_heat = ["Excellent", "Good", "Average", "Fair", "Poor"]
     heat = pd.DataFrame({"Heating Quality": hist_label_heat, "$ (USD)": hist_data_heat})
     ax = heat.plot.bar(x="Heating Quality", y="$ (USD)")
     st.bar_chart(heat, x="Heating Quality", y="$ (USD)")
 
+    st.subheader("Value Added by Number of Fireplaces")
     hist_data_fire = [10603.88432, 21207.76864, 31811.65296]
     hist_label_fire = [1,2,3]
     fire = pd.DataFrame({"Number of Fireplaces": hist_label_fire, "$ (USD)": hist_data_fire})
     ax = fire.plot.bar(x="Number of Fireplaces", y="$ (USD)")
     st.bar_chart(fire, x="Number of Fireplaces", y="$ (USD)")
 
+    st.subheader("Value Added by Number of Garage Size")
     hist_data_garage = [13710.65123, 27421.30247, 41131.9537, 54842.60493]
     hist_label_garage = [1,2,3,4]
     fire = pd.DataFrame({"Number of Garage Bays": hist_label_garage, "$ (USD)": hist_data_garage})
@@ -72,7 +77,7 @@ if __name__ == "__main__":
             delta="{:,.2f}".format(st.session_state["PriceChange"]))
 
 
-
+        st.subheader("Affect on Price by Feature")
         labels = st.session_state["PriceBreakdown"].keys()
         sizes = st.session_state["PriceBreakdown"].values()
         fire = pd.DataFrame({"Price Composition": labels, "$ (USD)": sizes})
