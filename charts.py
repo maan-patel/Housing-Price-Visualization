6eb90691488cb47d00dc5ec696562290515bd9aa
angus-lin <anguslin199@gmail.com>
2023-07-13 21:02:10 -0400

    fixed bugs

--- charts.py
+++ charts.py
@@ -8,27 +8,28 @@ def garage_chart(df, columns, coefficients):
     values = [coeff * x for x in range(int(df['GarageCars']['min'])+1, int(df['GarageCars']['max']+1))]
 
     chart_data = pd.DataFrame(
-        {"Value Added by Number of Cars Garage Holds": 
+        {"Number of Car Spaces in Garage": 
             [x for x in range(
-            int(df['GarageCars']['min']),
-            int(df['GarageCars']['max']))],
+            int(df['GarageCars']['min'])+1,
+            int(df['GarageCars']['max'])+1)],
         "Change in Price (USD)": values
         },
     )
 
-    st.bar_chart(chart_data, x = "Value Added by Number of Cars Garage Holds")
+    st.bar_chart(chart_data, x = "Number of Car Spaces in Garage")
 
 def fireplace_chart(df, columns, coefficients):
     coeff = coefficients[columns.get_loc('Fireplaces')]
     values = [coeff * x for x in range(int(df['Fireplaces']['min'])+1, int(df['Fireplaces']['max']+1))]
+    print(values)
 
     chart_data = pd.DataFrame(
-        {"Value Added by Fireplaces": 
+        {"Number of Fireplaces": 
             [x for x in range(
-            int(df['Fireplaces']['min']),
-            int(df['Fireplaces']['max']))],
+            int(df['Fireplaces']['min'])+1,
+            int(df['Fireplaces']['max'])+1)],
         "Change in Price (USD)": values
         },
     )
 
-    st.bar_chart(chart_data, x="Value Added by Fireplaces")
+    st.bar_chart(chart_data, x="Number of Fireplaces")
